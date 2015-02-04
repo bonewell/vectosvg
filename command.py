8b26af1fb440f9cab0207e72b869e9c7becb3382
Konstantin Kolodii <bone@land.ru>
2015-02-04 16:44:00 +0300

    Stairs isn't work. Need the checking of end stair

--- command.py
+++ command.py
@@ -1,6 +1,36 @@
 #!/usr/bin/env python
 
+import math
+
 from adapter import AdapterInterface
+
+# Helpers function. Need to remove Command interface into other file
+def translate(x1, y1, x2, y2, t):
+	dx = x2 - x1
+	dy = y2 - y1
+	if dx == 0:
+		tx = 0
+		ty = t * (dy / dy) * -1 # Oy is top to bottom
+	elif dy == 0:
+		tx = t * (dx / dx)
+		ty = 0
+	else:
+		tga = dx / dy
+		a = math.atan(tga)
+		tx = t * math.sin(a) * -1 # Oy is top to bottom
+		ty = t * math.cos(a) * -1 # Oy is top to bottom
+	return (tx, ty)
+
+def isborder(x, y, tx, ty, xg, yg):
+	return True
+	#if tx < 0:
+	#	return x >= gx
+	#	
+	#else:
+	#		
+	#else:
+	#	xg = x1
+
 
 class Command:
 	def __init__(self, params):
@@ -72,9 +102,15 @@ class StairsCommand(Command):
 		(x1, y1) = self.params[0].strip(',').split(',')
 		(x2, y2) = self.params[1].strip(',').split(',')
 		(x3, y3) = self.params[2].strip(',').split(',')
-		dx = int(x2) - int(x1)
-		dy = int(y2) - int(y1)
-		x4 = int(x3) + dx
-		y4 = int(y3) + dy
-		self.adapter.line(x1, y1, x2, y2)
-		self.adapter.line(x3, y3, x4, y4)
+		t = 4
+		(tx, ty) = translate(int(x1), int(y1), int(x3), int(y3), t)
+		xb = int(x1)
+		yb = int(y1)
+		xe = int(x2)
+		ye = int(y2)
+		while not isborder(xb, yb, tx, ty, int(x3), int(y3)):
+			self.adapter.line(xb, yb, xe, ye)
+			xb += tx
+			yb += ty
+			xe += tx
+			ye += ty
