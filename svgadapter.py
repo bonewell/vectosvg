6bb584313202f60ae6f0ccd302718c7f34ba706a
Konstantin Kolodii <bone@land.ru>
2015-02-23 17:19:20 +0300

    Change rotate, arrow

--- svgadapter.py
+++ svgadapter.py
@@ -8,50 +8,29 @@ from adapter import AdapterInterface
 class SvgAdapter(AdapterInterface):
 	def __init__(self, filename):
 		self.image = svgwrite.Drawing(filename, profile='full')
-		self.sc = 0
-		self.ac = 0
 		self.stroke = self.color('000000')
 		self.fill = self.color('-1')
 		self.opacity = 1.0
-#		self.defs()
+		self.defs()
+		self.root = self.image.add(self.image.g())
 
 	def __del__(self):
 		self.image.save()
 
-	def write(self, data):
-		pass
-		#self.f.write('%s\n' % data)
-
 	def defs(self):
-		pass
-		#self.arrow = self.image.marker((0, 5), (4, 3))
-		#self.arrow.add(self.image.circle((0, 0), r=5))
-		#self.image.defs.add(self.arrow)
-#<defs>
-#    <marker id="arrow"
-#      viewBox="0 0 10 10" refX="0" refY="5"
-#      markerUnits="strokeWidth"
-#      markerWidth="4" markerHeight="3"
-#      orient="auto">
-#      <path d="M 0 0 L 10 5 L 0 10 z" />
-#    </marker>
-#</defs>
-
-	def group(self):
-		return self.image.add(self.image.g())
+		self.marker = self.image.marker((0, 5), (4, 3), 'auto', markerUnits = "strokeWidth")
+		self.marker.viewbox(0, 0, 10, 10)
+		self.marker.add(self.image.path("M 0 0 L 10 5 L 0 10 z"))
+		self.image.defs.add(self.marker)
 
 	def size(self, w, h):
-		self.sc += 1
 		self.cx = int(w)/2
 		self.cy = int(h)/2
 		self.rect(0, 0, w, h)
 
 	def rotate(self, a):
-		self.ac += 1
-		self.a = float(a) * -1
-		templ = '<g transform="rotate(%s %s %s)" >'
-		data = templ % (self.a, self.cx, self.cy)
-		self.write(data)
+		alfa = float(a) * -1
+		self.root.rotate(alfa, (self.cx, self.cy))
 
 	def color(self, color):
 		return 'none' if color == '-1' else '#%s' % color
@@ -64,20 +43,19 @@ class SvgAdapter(AdapterInterface):
 
 	def opaque(self, v):
 		self.opacity = float(v) / 100
-		self.newgroup = True
 
 	def line(self, x1, y1, x2, y2):
-		line = self.image.add(self.image.line((x1, y1), (x2, y2)))
+		line = self.root.add(self.image.line((x1, y1), (x2, y2)))
 		line.stroke(self.stroke, width=1)
 
 	def polyline(self, points, w, dashed=False):
-		polyline = self.image.add(self.image.polyline(points))
+		polyline = self.root.add(self.image.polyline(points))
 		polyline.fill('none').stroke(self.stroke, width=w)
 		if dashed:
 			polyline.dasharray([5, 2])
 
 	def polygon(self, points):
-		polygon = self.image.add(self.image.polygon(points))
+		polygon = self.root.add(self.image.polygon(points))
 		polygon.fill(self.fill).stroke(self.stroke, width=1)
 
 	def ellipse(self, x1, y1, x2, y2):
@@ -85,27 +63,25 @@ class SvgAdapter(AdapterInterface):
 		ry = (int(y2) - int(y1)) / 2
 		cx = int(x1) + rx
 		cy = int(y1) + ry
-
-		ellipse = self.image.add(self.image.ellipse((cx, cy), (math.fabs(rx), math.fabs(ry))))
+		ellipse = self.root.add(self.image.ellipse((cx, cy), (math.fabs(rx), math.fabs(ry))))
 		ellipse.fill(self.fill).stroke(self.stroke, width=1)
 
 	def spline(self, points, w):
-		polyline = self.image.add(self.image.polyline(points))
+		polyline = self.root.add(self.image.polyline(points))
 		polyline.fill('none').stroke(self.stroke, width=w)
 
 	def arrow(self, points):
-		polyline = self.image.add(self.image.polyline(points))
+		polyline = self.root.add(self.image.polyline(points))
 		polyline.fill('none').stroke(self.stroke, width=1)
-#		polyline.set_markers(self.arrow)
+		polyline['marker-end'] = self.marker.get_funciri()
 
 	def rect(self, x1, y1, x2, y2):
 		w = int(x2) - int(x1)
 		h = int(y2) - int(y1)
-		rect = self.image.add(self.image.rect((x1, y1), (w, h)))
+		rect = self.root.add(self.image.rect((x1, y1), (w, h)))
 		rect.fill('none').stroke('none', width=0)
 
 	def text(self, x, y, text, size, font, a):
-		self.group()
 		if not a:
 			templ = '<text x="%s" y="%s" font-family="%s" font-size="%s" fill="#%s" stroke-width="0">%s</text>'
 			data = templ % (x, float(y) + float(size), font, size, self.stroke, cgi.escape(text))
@@ -113,4 +89,3 @@ class SvgAdapter(AdapterInterface):
 			a *= -1
 			templ = '<text x="%s" y="%s" font-family="%s" font-size="%s" fill="#%s" transform="rotate(%s %s %s)" stroke-width="0">%s</text>'
 			data = templ % (x, float(y) + float(size), font, size, self.stroke, a, x, y, cgi.escape(text))
-		self.write(data)
